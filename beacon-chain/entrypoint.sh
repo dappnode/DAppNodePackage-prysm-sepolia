fd03e540fb7ca7f288b25f72aff880a908173151
Tropicar <zerozept@protonmail.com>
2022-07-08 10:35:56 +0200

    Fix entrypoint and docker-compose
    Add TTD value, its required
    Change jwt name

--- beacon-chain/entrypoint.sh
+++ beacon-chain/entrypoint.sh
@@ -16,7 +16,9 @@ exec -c beacon-chain \
   --p2p-tcp-port=$P2P_TCP_PORT
   --p2p-udp-port=$P2P_UDP_PORT
   --http-web3provider=$HTTP_WEB3PROVIDER \
+  --suggested-fee-recipient=$FEE_RECIPIENT \
   --grpc-gateway-port=3500 \
-  --jwt-secret=/jwtsecret \
+  --jwt-secret=/jwt.hex \
+  --terminal-total-difficulty-override=$TTD_OVERRIDE \
   --grpc-gateway-corsdomain=$CORSDOMAIN \
   $EXTRA_OPTS
