da49e75d0dd2255361c5591350b8500d2f4bfa88
Tropicar <zerozept@protonmail.com>
2022-07-11 18:14:49 +0200

    Fix entrypoint
    The main problem was the order of the flags 
    Genesis flag and another merge flags
    No error message was indicated and some flags did not  work properly

--- beacon-chain/entrypoint.sh
+++ beacon-chain/entrypoint.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-if [[ -n $WEB3_BACKUP ]] && [[ $EXTRA_OPTS != *"--fallback-web3provider"* ]]; then
-  EXTRA_OPTS="--fallback-web3provider=${WEB3_BACKUP} ${EXTRA_OPTS}"
+if [[ -n $FEE_RECIPIENT ]] && [[ $EXTRA_OPTS != *"--suggested-fee-recipient"* ]]; then
+  EXTRA_OPTS="--suggested-fee-recipient=${FEE_RECIPIENT} ${EXTRA_OPTS}"
 fi
 
+
 exec -c beacon-chain \
-  --sepolia \
-  --accept-terms-of-use \
   --datadir=/data \
-  --genesis-state=/genesis.ssz \
+  --sepolia \
   --rpc-host=0.0.0.0 \
   --grpc-gateway-host=0.0.0.0 \
-  --monitoring-host=0.0.0.0 \
-  --p2p-local-ip=0.0.0.0 \
-  --p2p-tcp-port=$P2P_TCP_PORT
-  --p2p-udp-port=$P2P_UDP_PORT
-  --http-web3provider=$HTTP_WEB3PROVIDER \
-  --suggested-fee-recipient=$FEE_RECIPIENT \
   --grpc-gateway-port=3500 \
+  --grpc-gateway-corsdomain=$CORSDOMAIN \
+  --p2p-local-ip=0.0.0.0 \
+  --http-web3provider=$HTTP_WEB3PROVIDER \
+  --p2p-tcp-port=$P2P_TCP_PORT \
+  --p2p-udp-port=$P2P_UDP_PORT  \
+  --accept-terms-of-use \
   --jwt-secret=/jwt.hex \
   --terminal-total-difficulty-override=$TTD_OVERRIDE \
-  --grpc-gateway-corsdomain=$CORSDOMAIN \
+  --genesis-state=genesis.ssz \
   $EXTRA_OPTS
